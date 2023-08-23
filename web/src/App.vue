2490d4567acbacbe74408a38b8ff646d8924e0ba
sncswb-afk <xiayr@seas.upenn.edu>
2023-08-23 18:20:46 -0400

    4.2配置Mysql与注册登录模块

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,57 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+// import $ from 'jquery'
 
 export default {
   components: {
     NavBar
+  },
+  setup() {
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/token/",
+    //   type: "post",
+    //   data: {
+    //     username: 'xyr',
+    //     password: "pxyr",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5YjY1ZDNhNjU4Mzk0MTcxYTQwZGJjNzQzM2E0YjgwMiIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY5MjY1Mjc0OCwiZXhwIjoxNjkzODYyMzQ4fQ.fppJn1CkqG6NwPVC26Xy4HIGqtemM13ALidzDyOGAD4"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "xyr2",
+    //     password: "pxyr2",
+    //     confirmedPassword: "pxyr2",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 </script>
